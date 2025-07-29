19db42140b8ea29ec812d0fa43511ee70a7f37f8
TraciK <70920574+TraciK@users.noreply.github.com>
2025-07-29 19:24:49 +0000

    Finalized

--- umbrella.rb
+++ umbrella.rb
@@ -1,97 +1,83 @@
 require "http"
 require "json"
 require "dotenv/load"
-require "http"
 
-# Hidden variables
+puts "========================================"
+puts "    Will you need an umbrella today?"
+puts "========================================"
+puts
+puts "Where are you?"
+raw_location      = gets.chomp
+encoded_location  = raw_location.gsub(" ", "%20")
+
+# 1) Geocode the location
+maps_url =
+  "https://maps.googleapis.com/maps/api/geocode/json?" +
+  "address=#{encoded_location}&" +
+  "key=#{ENV.fetch("GMAPS_KEY")}"
+
+maps_resp     = HTTP.get(maps_url)
+maps_data     = JSON.parse(maps_resp.to_s)
+results       = maps_data.fetch("results")
+first_result  = results.at(0)
+loc_hash      = first_result.fetch("geometry").fetch("location")
+latitude      = loc_hash.fetch("lat")
+longitude     = loc_hash.fetch("lng")
+
+puts "Checking the weather at #{raw_location}...."
+puts "Your coordinates are #{latitude}, #{longitude}."
+
+# 2) Fetch the weather
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+pirate_weather_url =
+  "https://api.pirateweather.net/forecast/" +
+  "#{pirate_weather_api_key}/#{latitude},#{longitude}"
 
+weather_resp  = HTTP.get(pirate_weather_url)
+weather_data  = JSON.parse(weather_resp.to_s)
 
+# 3) Current temperature
+currently_hash = weather_data.fetch("currently")
+current_temp   = currently_hash.fetch("temperature")
+puts "It is currently #{current_temp}°F."
 
-# Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/Fuh1WZEWsG2BYST9ddTFgt9Vp2dV2TNt/41.8887,-87.6355"
-
-# Place a GET request to the URL
-raw_response = HTTP.get(pirate_weather_url)
-
-require "json"
-
-parsed_response = JSON.parse(raw_response)
-
-currently_hash = parsed_response.fetch("currently")
-
-current_temp = currently_hash.fetch("temperature")
-pp "========================================
-    Will you need an umbrella today?    
-    ========================================
-    Where are you?"
-
-user_location = gets.chomp.gsub(" ", "%20")
-
-pp user_location
-
-   maps_url =
-    "https://maps.googleapis.com/maps/api/geocode/json?" +
-    "address=#{user_location}&key=#{ENV.fetch("GMAPS_KEY")}"
-
-require "http"
-
-resp = HTTP.get(maps_url)
-
-pp resp.to_s
-
-raw_response = resp.to_s
-
-require "json"
-
-parsed_response = JSON.parse(raw_response)
-
-results = parsed_response.fetch("results")
-
-pp parsed_response.fetch("results")
-
-first_result = results.at(0)
-
-geo = first_result.fetch("geometry")
-
-loc = geo.fetch("location")
-
-latitude = loc.fetch("lat")
-longitude = loc.fetch("lng")
-
-parsed = JSON.parse(raw_response.to_s)
- hourly = parsed.fetch("hourly")
- data   = hourly.fetch("data")
-
-next_hour    = data.at(1)
- precip_prob  = next_hour.fetch("precipProbability")
-
- percentage   = (precip_prob * 100).round  
-
- minutely_hash = parsed_response.fetch("minutely", false)
+# 4) Minute-by-minute summary (if available)
+minutely_hash = weather_data.fetch("minutely", false)
 
 if minutely_hash
-  # Option 1: print the summary string
-  summary = minutely_hash.fetch("summary")
+  summary          = minutely_hash.fetch("summary")
   puts "Next hour: #{summary}"
 
-  # Option 2: compute the exact minute it stops
-  minutely_data = minutely_hash.fetch("data")
-  # drop the current minute so index 0 → 1 minute from now
-  upcoming     = minutely_data[1..-1]
-  stop_index   = upcoming.find_index { |minute_hash|
-    minute_hash.fetch("precipIntensity") <= 0.0
-  }
+  minutely_data    = minutely_hash.fetch("data")
+  upcoming_minutes = minutely_data[1..-1]
+  stop_index       = upcoming_minutes.find_index do |m|
+    m.fetch("precipIntensity") <= 0.0
+  end
 
+  if stop_index
+    minutes_until_stop = stop_index + 1
+    puts "Rain stops in #{minutes_until_stop} minutes."
+  else
+    puts "Rain continues for the next hour."
+  end
 end
 
-puts "Checking the weather at " + user_location.to_s + "...."
-puts "Your coordintes are #{latitude}, #{longitude}"
-puts "It is currently " + #{current_temp} + "."
-puts "Next hour: Rain is stopping in " + #{minutes_until_stop} + " min."
-puts "In 0 hours, there is a " + #{percentage} + "chance of precipitation."
-If percentage >= 15
-  pp "You might want to take an umbrella!"
+# 5) Hourly precipitation check
+hourly_data = weather_data.fetch("hourly").fetch("data")[1..12]
+any_precip  = false
+
+hourly_data.each do |hour_hash|
+  precip_prob   = hour_hash.fetch("precipProbability")
+  if precip_prob > 0.10
+    any_precip     = true
+    hours_from_now = ((Time.at(hour_hash.fetch("time")) - Time.now) / 3600).round
+    percentage     = (precip_prob * 100).round
+    puts "In #{hours_from_now} hours, there is a #{percentage}% chance of precipitation."
+  end
+end
+
+if any_precip
+  puts "You might want to take an umbrella!"
 else
-  pp "You don't need an umbrella."
+  puts "You probably won't need an umbrella today."
 end
